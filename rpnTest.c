ba1cd2bfeba2d9285c32af9833394cf8e8bf5215
Kaustav Chakraborty <kaustav.ron@gmail.com>
2015-02-05 14:22:30 +0530

    perform() and generateToken() method are tested

--- rpnTest.c
+++ rpnTest.c
@@ -201,3 +201,30 @@ void test_isOperator_should_return_0_for_greaterThan () {
 void test_isOperator_should_return_0_for_comma () {
 	assertEqual(isOperator(','), 0);
 }
+
+void test_perform_should_return_2_for_1_1_plus () {
+	Result res;
+	Stack stack = createStack();
+	String expression = "1 1 + ";
+	res = perform(stack, expression);
+	assertEqual(res.error, 1);
+	assertEqual(res.result, 2);
+}
+
+void test_perform_should_return_error_for_1_1_plus_plus () {
+	Result res;
+	Stack stack = createStack();
+	String expression = "1 1 + +";
+	res = perform(stack, expression);
+	assertEqual(res.error, 0);
+	assertEqual(res.result, 0x0);
+}
+
+void test_should_create_a_list_of_tokens () {
+	LinkedList list = createList();
+	String expr = "1 1 +";
+	generateToken(&list, expr);
+	assertEqual(((Token*)list.head->data)->type, 1);
+	assertEqual(((Token*)list.head->next->data)->type, 0);
+	assertEqual(((Token*)list.head->next->next->next->next->data)->type, 2);
+}
